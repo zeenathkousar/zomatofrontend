da82b3743ccf6c73bfebec439487e2c70155cdd1
zeenathkousar <95568153+zeenathkousar@users.noreply.github.com>
2024-01-06 18:28:29 +0530

    Update Login.jsx

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:3000/api/loginuser', {
+    const response = await fetch('https://backendzomatowebsite.onrender.com/api/loginuser', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
