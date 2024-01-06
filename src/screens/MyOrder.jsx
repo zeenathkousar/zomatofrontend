51a958cdab24d3501b9d14f9111952a61864021b
zeenathkousar <95568153+zeenathkousar@users.noreply.github.com>
2024-01-06 18:29:18 +0530

    Update MyOrder.jsx

--- src/screens/MyOrder.jsx
+++ src/screens/MyOrder.jsx
@@ -6,7 +6,7 @@ export default function MyOrder() {
   const [orderData, setorderData] = useState('')
   const fetchMyOrder=async()=>{
     console.log(localStorage.getItem('userEmail'))
-    await fetch('http://localhost:3000/api/myOrderData',{
+    await fetch('https://backendzomatowebsite.onrender.com/api/myOrderData',{
       method:'POST',
       headers:{
         'Content-Type':'application/json'
