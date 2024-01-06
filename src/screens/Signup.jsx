8b9b3e32f72be8d22654394705b5c8cbd72d9500
zeenathkousar <95568153+zeenathkousar@users.noreply.github.com>
2024-01-06 18:27:13 +0530

    Update Signup.jsx

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -6,7 +6,7 @@ const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch('http://localhost:3000/api/createuser', {
+        const response = await fetch('https://backendzomatowebsite.onrender.com/api/createuser', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
